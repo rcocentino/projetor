b3774e48ec8a3e5c12688f3fd16fc146334c179e
Dennis Correia <rcocentino@proton.me>
2022-05-11 12:37:48 -0300

    v3

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,7 @@
 </script>
 
 <template>
+    <p>Olá, tudo bem?</p>
     <div v-for="cliente in data" v-bind:id="`harsh${cliente.cnpj}`">
         <button v-bind:id="`uuid-${cliente.id}`" v-on:click="sendMessage(`${cliente.id}`)">{{ cliente.nome }}</button>
     </div>
